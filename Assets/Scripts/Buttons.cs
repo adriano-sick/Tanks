0c53f0e9a98efe8d23b950cc3bedb9db9ebcdf63
Adriano Siqueira <adriano_siqueira_silva@hotmail.com>
2020-08-07 02:19:22 -0300

    08-01

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{    
+    public GameObject mainCanvas;
+    public GameObject instructionCanvas;
+    public GameObject configurationCanvas;
+    public string button;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        if (button == "PlayButton")
+        {
+            PlayButton();
+        }
+
+        else if (this.button == "ExitButton")
+        {
+            QuitButton();
+        }
+
+        else if (button == "InstructionButton")
+        {
+            InstructionButton();
+        }
+
+        else if (this.button == "ConfigButton")
+        {
+            ConfigButton();
+        }
+
+        else if (this.button == "BackButton")
+        {
+            BackButton();
+        }
+    }
+
+   
+    void PlayButton()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    void InstructionButton()
+    {        
+        mainCanvas.SetActive(false);
+        configurationCanvas.SetActive(false);
+        instructionCanvas.SetActive(true);
+    }
+
+    void ConfigButton()
+    {
+        mainCanvas.SetActive(false);
+        instructionCanvas.SetActive(false);        
+        configurationCanvas.SetActive(true);
+    }
+
+    void BackButton()
+    {
+        instructionCanvas.SetActive(false);
+        configurationCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/UISettings.cs
@@ -0,0 +1,172 @@
+﻿using Complete;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class UISettings : MonoBehaviour
+{
+    public GameManager m_GameManager;
+    public GameObject tank;
+    public GameObject shell;
+    public GameObject mainCamera;
+    public AudioMixer masterMixer;    
+
+    private Dropdown dropdown;
+    private TankShooting tankShooting;
+    private TankHealth tankHealth;
+    private ShellExplosion tankDamage;
+    private Slider slider;
+    private float sfxLevel;
+    private float bgsLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown = GetComponent<Dropdown>();
+        tankShooting = tank.GetComponent<TankShooting>();
+        tankHealth = tank.GetComponent<TankHealth>();
+        tankDamage = shell.GetComponent<ShellExplosion>();
+        slider = gameObject.GetComponent<Slider>();
+
+        if (gameObject.name == "SFXSlider")
+        {
+            masterMixer.GetFloat("SFXVol", out sfxLevel);
+            slider.value = sfxLevel;
+        }
+
+        if (gameObject.name == "BGSSlider")
+        {
+            masterMixer.GetFloat("MusicVol", out bgsLevel);
+            slider.value = bgsLevel;
+        }        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.name == "RoundsDropdown")
+        {
+            RoundsNumber();           
+        }
+
+        if (gameObject.name == "MaxLaunchDropdown")
+        {
+            TankShooting();
+        }
+
+        if (gameObject.name == "MaxHealthDropdown")
+        {
+            TankHealth();
+        }
+
+        if (gameObject.name == "MaxDamageDropdown")
+        {
+            TankDamage();
+        }
+
+        if (gameObject.name == "SFXSlider")
+        {
+            SFXVol();
+        }
+
+        if (gameObject.name == "BGSSlider")
+        {
+            BGSVol();
+        }
+    }
+
+    void RoundsNumber()
+    {
+        if (dropdown.value == 0)
+        {
+            m_GameManager.m_NumRoundsToWin = 3;
+        }
+
+        if (dropdown.value == 1)
+        {
+            m_GameManager.m_NumRoundsToWin = 5;
+        }
+
+        if (dropdown.value == 2)
+        {
+            m_GameManager.m_NumRoundsToWin = 7;
+        }
+
+        if (dropdown.value == 3)
+        {
+            m_GameManager.m_NumRoundsToWin = 10;
+        }
+    }
+
+    void TankShooting()
+    {
+        if (dropdown.value == 0)
+        {
+            tankShooting.m_MaxLaunchForce = 30;
+        }
+
+        if (dropdown.value == 1)
+        {
+            tankShooting.m_MaxLaunchForce = 15;
+        }
+
+        if (dropdown.value == 2)
+        {
+            tankShooting.m_MaxLaunchForce = 60;
+        }
+    }
+
+    void TankHealth()
+    {
+        if (dropdown.value == 0)
+        {
+            tankHealth.m_StartingHealth = 100;
+        }
+
+        if (dropdown.value == 1)
+        {
+            tankHealth.m_StartingHealth = 50;
+        }
+
+        if (dropdown.value == 2)
+        {
+            tankHealth.m_StartingHealth = 200;
+        }
+    }
+
+    void TankDamage()
+    {
+        if (dropdown.value == 0)
+        {
+            tankDamage.m_MaxDamage = 100;
+        }
+
+        if (dropdown.value == 1)
+        {
+            tankDamage.m_MaxDamage = 10;
+        }
+
+        if (dropdown.value == 2)
+        {
+            tankDamage.m_MaxDamage = 200;
+        }
+    }
+
+    void SFXVol()
+    {
+        sfxLevel = slider.value;
+        masterMixer.SetFloat("SFXVol", sfxLevel);
+        masterMixer.SetFloat("DrivingVol", sfxLevel);
+    }
+
+    void BGSVol()
+    {
+        bgsLevel = slider.value;
+        masterMixer.SetFloat("MusicVol", bgsLevel);
+    }
+}
